285f289e376d4cca15e171b206d0aa2a9249dac8
KiraArgounova97 <kira.argounova97@gmail.com>
2018-11-20 04:26:37 +0900

    testing

---
+++ Chapter 2/2_1.cpp
@@ -0,0 +1,12 @@
+// 2_1.cpp -- displays name and address 
+
+#include <iostream>
+
+int main()
+{
+	using namespace std; 
+	cout << "Name: Kira Argounouv " << endl; 
+	cout << "Address: 18xx AR Drive" << endl; 
+	
+	return 0; 
+}
